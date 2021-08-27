0927b1746b7bfb9a09f3f66c6dd1edc03d605307
suribabu <suri.babu988@gmail.com>
2021-08-27 20:57:08 +0530

    custom color class created

---
+++ js/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#EA7633',
+                'secondary': '#F19447',
+                'third': '#F8B15A',
+                'siteblack': '#262626'
+            }
+        }
+    }
+}
